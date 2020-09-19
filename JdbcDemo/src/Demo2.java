2c82700e99dc8a32245c0466d5009746bc71541e
Chetan_Shinde <61513477+ShindeChetan@users.noreply.github.com>
2020-09-20 00:40:11 +0530

    Update Demo2.java

--- JdbcDemo/src/Demo2.java
+++ JdbcDemo/src/Demo2.java
@@ -27,7 +27,7 @@ class StudentDAO
 		try
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost/student","root","Bhai@2230");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/student","root","Password");
 		} catch (Exception ex)
 		{
 			System.out.println(ex);
