9b487cecd5bf8b6360b31b284052758feb5e97fe
Chetan Shinde <chetanshinde2230@gmail.com>
2020-09-20 00:38:04 +0530

    JDBC demo using DAO design pattern

---
+++ JdbcDemo/src/Demo2.java
@@ -0,0 +1,78 @@
+import java.sql.*;
+
+public class Demo2
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		StudentDAO dao=new StudentDAO();
+		//Student s1=dao.getStudent(3);
+		Student s2=new Student();
+		//System.out.println(s1.name);
+		s2.id=6;
+		s2.name="bhidu";
+		
+		dao.connect();
+		dao.addStudent(s2);
+	}
+
+}
+
+class StudentDAO
+{
+	Connection con=null;
+	public void connect()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost/student","root","Bhai@2230");
+		} catch (Exception ex)
+		{
+			System.out.println(ex);
+		} 
+	}
+	
+	public Student getStudent(int id)
+	{
+		try {
+			String query="select name from student where rollno="+id;
+			Student s=new Student();
+			s.id=id;
+			
+			Statement st=con.createStatement();
+			ResultSet rs= st.executeQuery(query);
+			rs.next();
+			String nam=rs.getString(1);
+			s.name=nam;
+			return s;
+		} catch (Exception ex)
+		{
+			// TODO Auto-generated catch block
+			System.out.println(ex);
+		}
+		return null;
+	}
+	
+	public void addStudent(Student s)
+	{
+		try {
+			String query="insert into student values (?,?)";
+			PreparedStatement pst=con.prepareStatement(query);
+			pst.setInt(1, s.id);
+			pst.setString(2, s.name);
+			pst.executeUpdate();
+			
+		} catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+}
+
+class Student
+{
+	int id;
+	String name;
+}
---
+++ JdbcDemo/src/JdbcDemoClass.java
@@ -0,0 +1,46 @@
+//Step 1
+import java.sql.*;
+
+/*
+ * Follow the 7 steps for jdbc connection
+ * 1.import the package-->java.sql.*
+ * 2.load & register the driver-->com.mysql.jdbc.Driver
+ * 3.establish the connection-->create object of Connection(interface)
+ * 4.create a statement
+ * 5.execute the query
+ * 6.processing of result
+ * 7.close the connection
+ */
+public class JdbcDemoClass
+{
+	public static void main(String[] args) throws Exception
+	{
+		String url="jdbc:mysql://localhost:3306/jdbc";
+		String uname="root";
+		String psw="Bhai@2230";
+		int id=4;
+		String name="Bouncer";
+		String query="insert into pract values(?,?)";
+		
+		//Step 2
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Step 3
+		Connection con=DriverManager.getConnection(url,uname,psw);
+		
+		//Step 4
+		PreparedStatement st=con.prepareStatement(query);
+		st.setInt(1, id);
+		st.setString(2, name);
+		//Step 5
+		int count= st.executeUpdate();
+		
+		//Step 6
+		System.out.println(count +"row/s affected");
+		
+		
+		//Step 7
+		st.close();
+		con.close();
+	}
+
+}
